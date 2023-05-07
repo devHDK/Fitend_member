d5316a8687ec6f504e524334b4f2dabfa100c356
devHDK <hyundo91082@gmail.com>
2023-05-07 17:54:03 +0900

    Add UserModel, Dio retry option

---
+++ lib/user/provider/auth_provider.dart
@@ -0,0 +1,18 @@
+import 'package:fitend_member/user/model/user_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final Ref ref;
+
+  AuthProvider({required this.ref}) {
+    ref.listen<UserModelBase?>(
+      userMeProvider,
+      (previous, next) {
+        if (previous != next) {
+          notifyListeners();
+        }
+      },
+    );
+  }
+}

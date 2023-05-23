be595a76ea575199e2cace6ed80878e927f1f553
devHDK <hyundo91082@gmail.com>
2023-05-23 13:27:42 +0900

    Add workoutScheduleRepository

---
+++ lib/schedule/repository/workout_schedule_repository.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:fitend_member/common/dio/dio.dart';
+import 'package:fitend_member/schedule/model/workout_schedule_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'workout_schedule_repository.g.dart';
+
+final workoutScheduleRepositoryProvider =
+    Provider<WorkoutScheduleRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return WorkoutScheduleRepository(dio);
+});
+
+@RestApi()
+abstract class WorkoutScheduleRepository {
+  factory WorkoutScheduleRepository(Dio dio) = _WorkoutScheduleRepository;
+
+  @GET('/workoutSchedules')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<WorkoutScheduleModel> getWorkoutSchedule({
+    @Body() required DateTime date,
+  });
+}
---
+++ lib/schedule/repository/workout_schedule_repository.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'workout_schedule_repository.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _WorkoutScheduleRepository implements WorkoutScheduleRepository {
+  _WorkoutScheduleRepository(
+    this._dio, {
+    this.baseUrl,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<WorkoutScheduleModel> getWorkoutSchedule(
+      {required DateTime date}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{r'accessToken': 'true'};
+    _headers.removeWhere((k, v) => v == null);
+    final _data = date;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<WorkoutScheduleModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/workoutSchedules',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = WorkoutScheduleModel.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}

d1c781be16983a6a4c4c435b494381e7609925ad
devHDK <hyundo91082@gmail.com>
2024-01-12 17:44:32 +0900

    Add Trainer repository

---
+++ lib/trainer/repository/trainer_repository.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:fitend_member/common/dio/dio.dart';
+import 'package:fitend_member/trainer/model/trainer_detail_model.dart';
+import 'package:fitend_member/trainer/model/trainer_list_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'trainer_repository.g.dart';
+
+final trainerRepositoryProvider = Provider<TrainerRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return TrainerRepository(dio);
+});
+
+@RestApi()
+abstract class TrainerRepository {
+  factory TrainerRepository(Dio dio) = _TrainerRepository;
+
+  @GET('/trainers')
+  Future<TrainerListModel> getTrainers();
+
+  @GET('/trainers/{id}')
+  Future<TrainerDetailModel> getTrainerDetail({
+    @Path('id') required int id,
+  });
+}
